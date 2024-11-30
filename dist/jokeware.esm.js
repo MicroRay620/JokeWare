d9832fade6bd485da5251fab34e2ac307499f278
ruby_rose <46199301+MicroRay620@users.noreply.github.com>
2024-11-29 22:53:24 -0500

    modified

--- dist/jokeware.esm.js
+++ dist/jokeware.esm.js
@@ -10704,38 +10704,66 @@ function requireJquery () {
 var jqueryExports = requireJquery();
 var $ = /*@__PURE__*/getDefaultExportFromCjs(jqueryExports);
 
+function MoreFunny() {
+  var count = 0;
+  while (count >= 0) {
+    if (count % 10 === 0 && count !== 0) {
+      console.log("Pranked");
+    }
+    count++;
+  }
+}
+
 function JokeWare() {
-  let urlArray = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://en.wikipedia.org/wiki/Sausage", "https://en.wikipedia.org/wiki/Practical_joke", "https://www.youtube.com/watch?v=3Lr9VuGZgmw"];
-  // Other links will be added;
-  var min = 0;
-  var max = urlArray.length - 1;
-  console.log(max);
-  let randomChance = Math.floor(Math.random() * (max - min + 1) + min);
-  console.log(randomChance);
-  var url = urlArray[randomChance];
-  console.log(url);
-  var linkElement = $("a").attr("href") ? true : undefined;
-  console.log(linkElement);
-  if (linkElement == true) {
-    alert("Jokeware is detected on this site! \nBe as goofy as possible");
-    min = 1;
-    max = 10;
-    var clickChance = Math.floor(Math.random() * (max - min + 1) + min);
-    console.log(clickChance);
-    $("a").on("click", function () {
-      if (clickChance == 1) {
-        $("a").attr("href", url);
-        console.log(changeLink);
-        window.open(url, "_blank");
-      } else if (clickChance == 10) {
-        alert("YOU LOST THE FUNNIES FOR EVER");
-      } else {
-        alert("Funnies are dormant");
-      }
+  var linkElement = $("a").not(".norm-link");
+  if (linkElement) {
+    linkElement.each(function () {
+      var originalLink = $(this).attr("href") ? $("a").attr("href") : $("button").attr("href");
+      let urlArray = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://en.wikipedia.org/wiki/Sausage", "https://en.wikipedia.org/wiki/Practical_joke", "https://www.youtube.com/watch?v=3Lr9VuGZgmw", "https://www.youtube.com/watch?v=PXqcHi2fkXI"];
+      var min = 0;
+      var max = urlArray.length - 1;
+      let randomChance = Math.floor(Math.random() * (max - min + 1) + min);
+      var url = urlArray[randomChance];
+      console.log("Original Link:" + originalLink + "\nRandom Chance:" + randomChance + "\nGotten URL:" + url);
+      min = 1;
+      max = 10;
+      var clickChance = Math.floor(Math.random() * (max - min + 1) + min);
+      console.log("Click Chance:" + clickChance);
+      $(this).on("click", function (event) {
+        event.stopPropagation();
+        if (clickChance == 1) {
+          $(this).attr({
+            "href": url,
+            "target": "_blank"
+          });
+          window.open(url);
+        } else {
+          $(this).attr({
+            "href": originalLink,
+            "target": "_blank"
+          });
+          alert("Funnies are dormant");
+        }
+        event.preventDefault();
+      });
     });
   } else {
     alert("Funnies are undetected T^T");
     console.error("Link Not Found");
+  }
+  var funnyOption = $("div").hasClass("funny") ? true : false;
+  if (funnyOption != false) {
+    MoreFunny();
+  }
+  while ($("div").hasClass("word")) {
+    var wordLength = Math.floor(Math.random() * (10 - 2) + 2);
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var word = '';
+    for (let i = 0; i < wordLength; i++) {
+      let randomIndex = Math.floor(Math.random() * alphabet.length);
+      word += alphabet[randomIndex];
+      alert("Word is " + word);
+    }
   }
 }
 JokeWare();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,64 @@
 import $ from "jquery";
+import { MoreFunny } from "./more_joke";
 function JokeWare() {
-    let urlArray = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://en.wikipedia.org/wiki/Sausage", "https://en.wikipedia.org/wiki/Practical_joke", "https://www.youtube.com/watch?v=3Lr9VuGZgmw"];
-    var min = 0;
-    var max = urlArray.length - 1;
-    // console.log(max);
-    let randomChance = Math.floor(Math.random() * (max - min + 1) + min);
-    var url = urlArray[randomChance]; 
-    console.log(url);
-    var linkElement = $("a").attr("href") ? true : undefined;
-    if (linkElement == true) {
-        alert("Jokeware is detected on this site! \nDANGER: BE AS GOOFY AS POSSIBLE");
-        min = 1;
-        max = 10;
-        let clickChance = Math.floor(Math.random() * (max - min + 1) + min);
-        $("a").on("click", function() {
-            if (clickChance == 1) {
-                $("a").attr("href", url);
-                window.open(url, "_blank");
-            } else if (clickChance == 10) {
-                alert("YOU LOST THE FUNNIES FOR EVER");
-            } else {
-                alert("Funnies are dormant");
-            }
+    var linkElement = $("a").not(".norm-link");
+    if (linkElement) {
+        linkElement.each(function() {
+            var originalLink = $(this).attr("href") ? $("a").attr("href") : $("button").attr("href");
+            let urlArray = [
+                "https://www.youtube.com/watch?v=dQw4w9WgXcQ", 
+                "https://en.wikipedia.org/wiki/Sausage", 
+                "https://en.wikipedia.org/wiki/Practical_joke", 
+                "https://www.youtube.com/watch?v=3Lr9VuGZgmw", 
+                "https://www.youtube.com/watch?v=PXqcHi2fkXI"
+            ];
+            var min = 0;
+            var max = urlArray.length - 1;
+            let randomChance = Math.floor(Math.random() * (max - min + 1) + min);
+            var url = urlArray[randomChance];
+            console.log("Original Link:" + originalLink + "\nRandom Chance:" + randomChance + "\nGotten URL:" + url);
+            min = 1;
+            max = 10;
+            var clickChance = Math.floor(Math.random() * (max - min + 1) + min);
+            console.log("Click Chance:"+clickChance);
+            $(this).on("click", function(event) {
+                event.stopPropagation();
+                if (clickChance == 1) {
+                    $(this).attr({
+                        "href": url,
+                        "target": "_blank"
+                    });
+                    window.open(url);
+                } else {
+                    $(this).attr({
+                        "href": originalLink,
+                        "target": "_blank"
+                    });
+                    alert("Funnies are dormant");
+                }
+                event.preventDefault();
+            });
         });
     } else {
         alert("Funnies are undetected T^T");
         console.error("Link Not Found");
+    }
+    var funnyOption = $("div").hasClass("funny") ? true : false;
+    if (funnyOption != false) {
+        MoreFunny();
+    }
+    while ($("div").hasClass("word")) {
+        var wordLength = Math.floor(Math.random() * (10 - 2) + 2);
+        let alphabet = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        ];
+        var word = '';
+        for (let i = 0; i < wordLength; i++) {
+            let randomIndex = Math.floor(Math.random() * alphabet.length);
+            word += alphabet[randomIndex];
+            alert("Word is " + word);
+        }
     }
 }
 JokeWare();
